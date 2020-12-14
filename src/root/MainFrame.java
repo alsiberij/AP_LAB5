e99957d85d5876fef19e583b5849f08ca97f6ba2
alsiberij <alsiberij@gmail.com>
2020-12-14 17:34:57 +0300

    Menu listener added

--- src/root/MainFrame.java
+++ src/root/MainFrame.java
@@ -1,11 +1,31 @@
 package root;
 
 import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
 public class MainFrame extends JFrame {
 
     public static void main(String[] args) {
 
+    }
+
+    private class PlotMenuListener implements MenuListener {
+        @Override
+        public void menuSelected(MenuEvent e) {
+            showAxisMI.setEnabled(fileLoaded);
+            showMarkersMI.setEnabled(fileLoaded);
+        }
+
+        @Override
+        public void menuDeselected(MenuEvent e) {
+
+        }
+
+        @Override
+        public void menuCanceled(MenuEvent e) {
+
+        }
     }
 
     private static final int WIDTH = 700;
